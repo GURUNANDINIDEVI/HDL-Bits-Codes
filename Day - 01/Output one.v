491ee949f2e6637d4a54a38ea84877fd6dec2c76
GURUNANDINIDEVI <154117298+GURUNANDINIDEVI@users.noreply.github.com>
2025-05-20 14:39:33 +0530

    Output one

---
+++ Day - 01/Output one.v
@@ -0,0 +1,12 @@
+////////////////////////////////////////
+// T.Guru Nandini Devi
+// nandinidevitekumudi@gmail.com
+// submission date: 20-05-2025
+/////////////////////////////////////////
+//Build a circuit with no inputs and one output. That output should always drive 1 (or logic high).
+module top_module( output one );
+
+// Insert your code here
+    assign one = 1'b1;
+
+endmodule
