14e4119454f2a8020e939115165b7b2a9ed9480e
Tekumudi Guru Nandini Devi <154117298+GURUNANDINIDEVI@users.noreply.github.com>
2025-05-21 18:05:41 +0530

    output zero_code

--- Day - 02/output zero_code.v
+++ Day - 02/output zero_code.v
@@ -1,3 +1,9 @@
+////////////////////////////////////////
+// T.Guru Nandini Devi
+// nandinidevitekumudi@gmail.com
+// submission date: 21-05-2025
+/////////////////////////////////////////
+//Problem Statement - 02 : Build a circuit with no inputs and one output that outputs a constant 0
 module top_module(
     output zero
 );
