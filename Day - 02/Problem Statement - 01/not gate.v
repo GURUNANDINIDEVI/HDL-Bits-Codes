f69679267bf50ef5cb0d5550a0ce01b1d2b218c7
Tekumudi Guru Nandini Devi <154117298+GURUNANDINIDEVI@users.noreply.github.com>
2025-05-22 21:47:59 +0530

    Problem Statement-1 code

---
+++ Day - 02/Problem Statement - 01/not gate.v
@@ -0,0 +1,12 @@
+//////////////////////////////////////
+///T.Guru Nandini Devi             ///
+///nandinidevitekumudi@gmail.com   ///
+///Submission Date: 22-05-2025     ///
+//////////////////////////////////////
+//Create a module that implements a NOT gate.
+module top_module(
+  input in, 
+  output out 
+);
+assign out= ~in;
+endmodule
