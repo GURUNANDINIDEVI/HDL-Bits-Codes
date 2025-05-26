e6f23963aee402f13f92a07542a293e4d02ad17c
Tekumudi Guru Nandini Devi <154117298+GURUNANDINIDEVI@users.noreply.github.com>
2025-05-26 22:46:26 +0530

    And_gate

---
+++ Day - 03/Problem Statement - 01/And_gate.v
@@ -0,0 +1,12 @@
+//////////////////////////////////////
+///T.Guru Nandini Devi             ///
+///nandinidevitekumudi@gmail.com   ///
+///Submission Date: 26-05-2025     ///
+//////////////////////////////////////
+//Create a module that implements an AND gate.
+module top_module( 
+    input a, 
+    input b, 
+    output out );
+    assign out = a & b;
+endmodule
