5a2922eb82704cffaa11bc7ede5c0c676c38869f
Tekumudi Guru Nandini Devi <154117298+GURUNANDINIDEVI@users.noreply.github.com>
2025-06-02 09:34:06 +0530

    vector 5 code

---
+++ Day - 09/Problem Statement - 02/vector 5.v
@@ -0,0 +1,16 @@
+//////////////////////////////////////
+///T.Guru Nandini Devi             ///
+///nandinidevitekumudi@gmail.com   ///
+///Submission Date: 01-06-2025     ///
+//////////////////////////////////////
+//The top vector is a concatenation of 5 repeats of each input
+//The bottom vector is 5 repeats of a concatenation of the 5 inputs
+module top_module (
+    input a, b, c, d, e,
+    output [24:0] out );//
+
+    // The output is XNOR of two vectors created by 
+    // concatenating and replicating the five inputs.
+    assign out = ~{ {5{a}}, {5{b}}, {5{c}}, {5{d}}, {5{e}} } ^ {5{a,b,c,d,e}};
+
+endmodule
