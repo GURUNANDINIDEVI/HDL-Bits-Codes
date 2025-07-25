71296e55ff146ee07b4a689a9bd9ad57d2eb1a47
Tekumudi Guru Nandini Devi <154117298+GURUNANDINIDEVI@users.noreply.github.com>
2025-07-25 12:04:18 +0530

    ground

---
+++ Day - 13/Problem Statement-02/ground.v
@@ -0,0 +1,9 @@
+//////////////////////////////////////
+///T.Guru Nandini Devi             ///
+///nandinidevitekumudi@gmail.com   ///
+//////////////////////////////////////
+//Implement Ground circuit
+module top_module (
+    output out);
+   assign out=1'b0;
+endmodule
