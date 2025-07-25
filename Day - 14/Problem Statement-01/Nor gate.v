eadf219c62741795d07ec721229cb98710dc2f08
Tekumudi Guru Nandini Devi <154117298+GURUNANDINIDEVI@users.noreply.github.com>
2025-07-25 12:17:32 +0530

    Nor gate

---
+++ Day - 14/Problem Statement-01/Nor gate.v
@@ -0,0 +1,10 @@
+//////////////////////////////////////
+///T.Guru Nandini Devi             ///
+///nandinidevitekumudi@gmail.com   ///
+//////////////////////////////////////
+module top_module (
+    input in1,
+    input in2,
+    output out);
+    assign out = ~(in1 | in2);
+endmodule
