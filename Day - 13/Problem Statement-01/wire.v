4a0ebc57ea4ac7bc78745f2058f318fa772ec822
Tekumudi Guru Nandini Devi <154117298+GURUNANDINIDEVI@users.noreply.github.com>
2025-07-25 11:55:40 +0530

    wire(combinational)

---
+++ Day - 13/Problem Statement-01/wire.v
@@ -0,0 +1,13 @@
+//////////////////////////////////////
+///T.Guru Nandini Devi             ///
+///nandinidevitekumudi@gmail.com   ///
+///Submission Date: 25-07-2025     ///
+//////////////////////////////////////
+//Implement the wire circuit
+module top_module (
+    input in,
+    output out);
+   assign out = in;
+endmodule
+
+
