dee274557c5d7062b7e7a25620894b872d973a21
Tekumudi Guru Nandini Devi <154117298+GURUNANDINIDEVI@users.noreply.github.com>
2025-09-12 15:12:26 +0530

    kmap(2012)

---
+++ Day - 30/Problem Statement - 01/kmap(2012).v
@@ -0,0 +1,8 @@
+//T.Guru Nandini Devi
+//nandinidevitekumudi@gmail.com 
+module top_module (
+    input [4:1] x,
+    output f
+); 
+    assign f = (x[3]& ~x[1]) | (~x[2] & ~x[4]) | (x[3] & x[4] & x[2]);
+endmodule
