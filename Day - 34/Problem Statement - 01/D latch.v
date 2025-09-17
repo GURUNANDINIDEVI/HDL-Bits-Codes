d991b013f2e0f2d9ed081e67267b6ac005177029
Tekumudi Guru Nandini Devi <154117298+GURUNANDINIDEVI@users.noreply.github.com>
2025-09-17 21:28:06 +0530

    D latch

---
+++ Day - 34/Problem Statement - 01/D latch.v
@@ -0,0 +1,14 @@
+//T.Guru Nandini Devi
+//nandinidevitekumudi@gmail.com
+module top_module (
+    input d, 
+    input ena,
+    output reg q
+);
+
+    always @(*) begin
+    if (ena)
+        q <= d;
+end
+
+endmodule
