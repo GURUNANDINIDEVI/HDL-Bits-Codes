f7dc54e2e6f65a110aee42bc17b191335e44b52e
Tekumudi Guru Nandini Devi <154117298+GURUNANDINIDEVI@users.noreply.github.com>
2025-09-20 15:12:29 +0530

    Combine circuit A&B

---
+++ Day - 18/Problem Statement - 03/Combine circuit A&B.v
@@ -0,0 +1,5 @@
+//T.Guru Nandini Devi
+//nandinidevitekumudi@gmail.com
+module top_module (input x, input y, output z);
+    assign z = (((x ^ y) & x) | (~(x ^ y))) ^ (((x ^ y) & x) & (~(x ^ y)));
+endmodule
