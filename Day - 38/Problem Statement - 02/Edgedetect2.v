d60e6b5a99e2e57c3bac5577b6021e83e4660159
Tekumudi Guru Nandini Devi <154117298+GURUNANDINIDEVI@users.noreply.github.com>
2025-09-29 20:51:03 +0530

    Edgedetect2

---
+++ Day - 38/Problem Statement - 02/Edgedetect2.v
@@ -0,0 +1,15 @@
+//T.Guru Nandini Devi
+//nandinidevitekumudi@gmail.com
+module top_module (
+    input clk,
+    input [7:0] in,
+    output [7:0] anyedge
+  );
+    
+  reg [7:0] in_last;
+  always @(posedge clk) begin
+      in_last <= in;
+      anyedge <= in ^ in_last;
+  end 
+
+endmodule
