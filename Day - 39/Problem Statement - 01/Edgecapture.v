8b59ac6e8b26bd6cd23f2fec386eb8aff34868f6
Tekumudi Guru Nandini Devi <154117298+GURUNANDINIDEVI@users.noreply.github.com>
2025-09-30 23:08:05 +0530

    Edgecapture

---
+++ Day - 39/Problem Statement - 01/Edgecapture.v
@@ -0,0 +1,21 @@
+//T.Guru Nandini Devi
+//nandinidevitekumudi@gmail.com
+module top_module (
+    input clk,
+    input reset,
+    input [31:0] in,
+    output [31:0] out
+);
+  
+  reg [31:0] in_last;	
+  
+  always @(posedge clk) begin
+      in_last <= in;
+      if(reset) 
+        out <= 0;
+      else begin
+        out <= out | (in_last & ~in);
+      end
+  end
+
+endmodule
